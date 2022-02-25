c002a6b78f39d31ce4ed01f415b15de63d5e4765
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-02-24 19:35:17 -0500

    Cleaned, added line goal detection fn

--- include/ClassBaseFns.h
+++ include/ClassBaseFns.h
@@ -44,6 +44,7 @@ string parseInt(vision::signature& c){
 
 //All the functions that are based on classes
 namespace ClassFns {
+  
   FN_WITH_APA_SIG_NO_ARG(allowVisionAlign)
     wc.setExitDist(24.0);
   }
@@ -95,35 +96,7 @@ namespace ClassFns {
     liftCtrllr.subIndex();
     waitForLiftFinish(false);
   }
-  // FN_WITH_APA_SIG_NO_ARG(raiseBackLiftByOne)
-  //   backLiftCtrllr.done = false;
-  //   backLiftCtrllr.freePress();
-  //   backLiftCtrllr.addIndex();
-  // }
-  // FN_WITH_APA_SIG_NO_ARG(lowerBackLiftByOne)
-  //   backLiftCtrllr.freePress();
-  //   backLiftCtrllr.done = false;
-  //   backLiftCtrllr.subIndex();
-  // }
-  // FN_WITH_APA_SIG_NO_ARG(waitForBackLiftFinish)
-  //   //cout << liftCtrllr.isDone() << endl;
-  //   while(!backLiftCtrllr.isDone()){
-  //     //cout << liftCtrllr.isDone() << endl;
-  //     s(10);
-  //   }
-  // }
-  // FN_WITH_APA_SIG_NO_ARG(raiseBackLiftByOneWait)
-  //   backLiftCtrllr.freePress();
-  //   backLiftCtrllr.done = false;
-  //   backLiftCtrllr.addIndex();
-  //   waitForBackLiftFinish(false);
-  // }
-  // FN_WITH_APA_SIG_NO_ARG(lowerBackLiftByOneWait)
-  //   backLiftCtrllr.freePress();
-  //   backLiftCtrllr.subIndex();
-  //   backLiftCtrllr.done = false;
-  //   waitForBackLiftFinish(false);
-  // }
+  
   FN_WITH_APA_SIG_NO_ARG(clipGoal)
     goalHolder->close();
     //wc.addGoal();
@@ -138,225 +111,32 @@ namespace ClassFns {
   FN_WITH_APA_SIG_NO_ARG(unclipLiftGoal)
     liftGoalHolder.open();
   }
-  //Really old functions
-  // FN_WITH_APA_SIG_NO_ARG(alignGoalFront)
-  //   //Maybe put a PD loop in here
-  //   goalFront.test();
-  //   goalBack.test();
-  //   double estimateDif = 0.0;
-  //   double lastEst = 0.0;
-  //   cout << goalFront.objectDistance(inches) << endl;
-  //   cout << goalFront.objectDistance(inches) << endl;
-  //   while(goalFront.objectDistance(mm) > 55 || goalBack.objectDistance(mm) > 55 || !goalFront.isObjectDetected() || !goalBack.isObjectDetected()){
-  //     wc.moveLeft(-30);
-  //     wc.moveRight(-30);
-  //     goalFront.test();
-  //     goalBack.test();
-  //     bool frontDetect = goalFront.isObjectDetected() && goalFront.objectDistance(inches) < 12.0;
-  //     bool backDetect = goalBack.isObjectDetected() && goalBack.objectDistance(inches) < 12.0;
-  //     double dif = goalFront.objectDistance(mm) - goalBack.objectDistance(mm);
-  //     if(goalFront.isObjectDetected() && goalBack.isObjectDetected()){
-
-  //       if(dif > 10){ // goalBack (right) is closer
-  //         wc.moveRight(-30 + dif * 0.75);
-  //       }
-  //       if(dif < -10){ // goalBack (right) is closer
-  //         wc.moveLeft(-30 + -dif * 0.75);
-  //       }
-  //     }
-  //     while(!frontDetect && backDetect){
-  //       wc.moveLeft(-20);
-  //       goalFront.test();
-  //       goalBack.test();
-  //       frontDetect = goalFront.isObjectDetected() && goalFront.objectDistance(inches) < 12.0;
-  //       backDetect = goalBack.isObjectDetected() && goalBack.objectDistance(inches) < 12.0;
-  //       goalFront.sleep();
-  //     }
-  //     wc.moveLeft(-30);
-  //     wc.moveRight(-30);
-  //     while(!backDetect && frontDetect){
-  //       wc.moveLeft(-20);
-  //       goalFront.test();
-  //       goalBack.test();
-  //       frontDetect = goalFront.isObjectDetected() && goalFront.objectDistance(inches) < 12.0;
-  //       backDetect = goalBack.isObjectDetected() && goalBack.objectDistance(inches) < 12.0;
-  //       goalFront.sleep();
-  //     }
-  //     wc.moveLeft(-30);
-  //     wc.moveRight(-30);
-  //     while(!backDetect && !frontDetect){
-  //       wc.moveLeft(-10);
-  //       wc.moveRight(10);
-  //       goalFront.test();
-  //       goalBack.test();
-  //       frontDetect = goalFront.isObjectDetected() && goalFront.objectDistance(inches) < 12.0;
-  //       backDetect = goalBack.isObjectDetected() && goalBack.objectDistance(inches) < 12.0;
-  //       goalFront.sleep();
-  //     }
-  //     goalFront.sleep();
-  //     cout << goalFront.objectDistance(mm) << ", " << goalBack.objectDistance(mm) << endl;
-  //   }
-  //   wc.moveLeft(-30);
-  //   wc.moveRight(-30);
-  //   s(300);
-  //   wc.coastBrake();
-  //   goalFront.endFiltering();
-  // }
-
-  //Functions I'm trying to replace
-  // FN_WITH_APA_SIG(alignGoalFront, vision::signature&)
-  //   vision::signature& code = amnt;
-  //   goalFrontVision.takeSnapshot(code);
-  //   vision::object* largestObject = &goalFrontVision.largestObject;
-  //   cout << largestObject->height << endl;
-  //   while(!largestObject->exists){
-  //     goalFrontVision.takeSnapshot(code);
-  //     largestObject = &goalFrontVision.largestObject;
-  //     wc.moveLeft(10);
-  //     wc.moveRight(-10);
-  //     s(10);
-  //   }
-  //   s(700);
-  //   vision::object* secondLargest;
-  //   if(goalFrontVision.objectCount > 1 && goalFrontVision.objects[1].height > 60){
-  //     secondLargest = &goalFrontVision.objects[1];
-  //   }
-  //   else {
-  //     secondLargest = largestObject;
-  //   }
-  //   goalFront.test();
-  //   goalBack.test();
-  //   double lastFront = 100;
-  //   double lastBack = 100;
-  //   while(largestObject->exists){
-  //     wc.moveLeft(-20);
-  //     wc.moveRight(-20);
-  //     if(largestObject == secondLargest){
-  //       //Largest is on left side
-  //       if(largestObject->centerX > 150){
-  //         wc.moveRight(0);
-  //       }
-  //       //Largest is on right side
-  //       else if(largestObject->centerX < 150){
-  //         wc.moveLeft(0);
-  //       }
-  //     }
-  //     else if (secondLargest->centerX < 245 && secondLargest->centerX > 180){
-  //       wc.moveRight(-20 - (245 - secondLargest->centerX) / 10.0);
-  //     }
-  //     else if(secondLargest->centerX > 50 && secondLargest->centerX < 120){
-  //       wc.moveLeft(-20 - (secondLargest->centerX - 50) / 10.0);
-  //     }
-      
-  //     else if(largestObject->centerX > 245){
-  //       wc.moveRight(-20 - (largestObject->centerX - 240) / 10.0);
-  //     }
-  //     else if(largestObject->centerX < 50){
-  //       wc.moveLeft(-20 - (50 - largestObject->centerX) / 10.0);
-  //     }
-  //     // else if(!goalFront.isObjectDetected() || goalFront.objectDistance(inches) > 8.0){
-  //     //   cout << "Left dist no obj" << endl;
-  //     //   wc.moveLeft(0);
-  //     // }
-  //     // else if(!goalBack.isObjectDetected() || goalBack.objectDistance(inches) > 8.0){
-  //     //   cout << "Right dist no obj" << endl;
-  //     //   wc.moveRight(0);
-  //     // }
-  //     // else {
-  //     //   cout << "Changing Based on dists" << endl;
-  //     //   wc.moveLeft(-10 + (goalFront.objectDistance(mm) - goalBack.objectDistance(mm)));
-  //     //   wc.moveRight(-10 - (goalFront.objectDistance(mm) - goalBack.objectDistance(mm)));
-  //     // }
-  //     goalFront.test();
-  //     goalBack.test();
-  //     if(goalFront.objectDistance(mm) < 55 && goalBack.objectDistance(mm) < 55 && goalFront.isObjectDetected() && goalBack.isObjectDetected()){
-  //       cout << "Near Front: " << goalFront.objectDistance(mm) << ", " << goalBack.objectDistance(mm) << endl;
-  //       break;
-  //     }
-  //     // if(goalFront.isObjectDetected() && goalFront.objectDistance(mm) < 55 && !goalBack.isObjectDetected() && lastBack < 65){
-  //     //   break;
-  //     // }
-  //     // if(goalBack.isObjectDetected() && goalBack.objectDistance(mm) < 55 && !goalFront.isObjectDetected() && lastFront < 65){
-  //     //   break;
-  //     // }
-  //     if(frontCounter.pressing()){
-  //       cout << "Goal Found" << endl;
-  //       break;
-  //     }
-  //     if(goalBack.isObjectDetected()){
-  //       lastBack = goalBack.objectDistance(mm);
-  //     }
-  //     if(goalFront.isObjectDetected()){
-  //       lastFront = goalFront.objectDistance(mm);
-  //     }
-  //     goalBack.sleep();
-  //     goalFrontVision.takeSnapshot(code);
-  //     largestObject = &goalFrontVision.largestObject;
-  //     if(!largestObject->exists){
-  //       cout << "Nothing found" << endl;
-  //       break;
-  //     }
-  //     if(goalFrontVision.objectCount > 1 && goalFrontVision.objects[1].height > 60){
-  //       secondLargest = &goalFrontVision.objects[1];
-  //     }
-  //     else {
-  //       secondLargest = largestObject;
-  //     }
-  //   }
-  //   goalFront.endFiltering();
-  //   goalBack.endFiltering();
-  //   wc.moveLeft(-30);
-  //   wc.moveRight(-30);
-  //   s(350);
-  //   wc.coastBrake();
-  // }
-  // FN_WITH_APA_SIG(alignGoalBack, vision::signature&)
-  //   vision::signature& code = amnt;
-  //   goalDetector.takeSnapshot(code);
-  //   auto* largestObject = &goalDetector.largestObject;
-  //   if(!largestObject->exists){
-  //     cout << "No obj" << endl;;
-  //     return;
-  //   }
-  //   int time = 0;
-  //   while(largestObject->exists){
-  //     wc.moveLeft(20);
-  //     wc.moveRight(20);
-  //     if(largestObject->width > 100){
-  //       if(largestObject->centerX < 150){
-  //         wc.moveRight(20 - (150 - largestObject->centerX) / 4);
-  //       }
-  //       else if(largestObject->centerX > 150){
-  //         wc.moveLeft(20 + (150 - largestObject->centerX) / 4);
-  //       }
-  //     }
-  //     else {
-  //       if(largestObject->centerX > 190){
-  //         wc.moveLeft(20 + (190 - largestObject->centerX) / 4);
-  //       }
-  //       else if(largestObject->centerX < 110){
-  //         wc.moveRight(20 - (110 - largestObject->centerX) / 4);
-  //       }
-  //     }
-  //     if(backGoalDetect.pressing() == 10){
-  //       break;
-  //     }
-  //     if(time >= 3000){
-  //       break;
-  //     }
-  //     s(10);
-  //     time += 10;
-
-  //     goalDetector.takeSnapshot(code);
-  //     largestObject = &goalDetector.largestObject;
-  //     //cout << largestObject->centerX << ", " << largestObject->originX << ", " << largestObject->width << endl;
-  //   }
-  //   cout << "Done" << endl;
-  //   wc.moveLeft(20);
-  //   wc.moveRight(20);
-  //   s(500);
-  //   wc.coastBrake();
-  // }
+  FN_WITH_APA_SIG_NO_ARG(useLineGoalDetect)
+    threadFns.push_back([](){
+      while(1){
+        if(frontCounter.pressing()){
+          clipLiftGoal();
+          s(300);
+          if(wc.isMoving()){
+            wc.forceEarlyExit();
+          }
+          break;
+        }
+        s(10);
+      }
+    });
+  }
+  FN_WITH_APA_SIG_NO_ARG(useLineGoalDetectNoExit)
+    threadFns.push_back([](){
+      while(1){
+        if(frontCounter.pressing()){
+          clipLiftGoal();
+          break;
+        }
+        s(10);
+      }
+    });
+  }
   //Needed for prog skills (maybe)
   FN_WITH_APA_SIG_NO_ARG(balanceBot)
     PID speedGetter = PID(1.5, 0.001, 0.1);
