e9d1b6e47f7e6a1dcc860d4b35da8653ae865ebc
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-03-22 17:42:44 -0400

    Rand lvgl stuff

--- include/BrainOS/BotTracker.h
+++ include/BrainOS/BotTracker.h
@@ -7,9 +7,7 @@ drawBotPos() {
   lv_chart_set_type(graph, LV_CHART_TYPE_AREA | LV_CHART_TYPE_NONE);
   lv_obj_set_size(graph, 220, 220);
   lv_obj_set_pos(graph, 10, 10);
-  static lv_obj_t* lines = lv_line_create(lv_scr_act(), NULL);
-  lv_point_t a[] = {wc.botPos(), wc.botPos()};
-  lv_line_set_points(lines, a, 2);
+  
   while(1) {
     this_thread::sleep_for(5);
   }
