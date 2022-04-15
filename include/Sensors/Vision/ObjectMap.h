ad27402ff709e2be0aa5819f945a816906ce9645
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-04-15 14:46:27 -0400

    Do more vision stuff

--- include/Sensors/Vision/ObjectMap.h
+++ include/Sensors/Vision/ObjectMap.h
@@ -5,20 +5,26 @@ struct BasicObject {
   EMA width = EMA(0.95); // Give 95% trust to old measurements in case of object splitting
   EMA height = EMA(0.95);
   void addData(vision::object& object){
-    origin.update({object.originX, object.originY});
+    origin.update({static_cast<double>(object.originX), static_cast<double>(object.originY)});
     width.update(object.width);
     height.update(object.height);
   }
 };
 struct ObjectMap {
   CircFieldObject object;
-  safearray<vision::object, VISION_MAX_OBJECTS> objects;
-  safearray<vision::object, VISION_MAX_OBJECTS> lastObjects;
+  vision& sensor;
+  safearray<BasicObject, VISION_MAX_OBJECTS> objects;
   //Merge two objects if their detections are very close and the sizes are similar
   //Will require some pointer hacks to get around the const& types 
   void mergeClose(){
     
   }
-  void update();
+  void update(){
+    sensor.takeSnapshot(*object.color);
+    safearray<vision::object, VISION_MAX_OBJECTS>& arr = sensor.objects;
+    for(int i = 0; i < objects.getLength(); i++){
+      vision::object* closestObj = NULL;
+    }
+  }
 
 };
