b5dfe4cc2f4895a327bdb9a3a30edda46fdafcf5
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-05-04 21:51:44 -0400

    Move refines to VisionOdom.h

--- include/Sensors/VisionOdom.h
+++ include/Sensors/VisionOdom.h
@@ -6,6 +6,120 @@ extern GPS_Share share;
 class VisionOdom {
   vision* sensor;
   double mountHeight, mountAngle, mountRotation;
+  struct Refine {
+  private:
+    static double getDistEst(double sensorHeight, double distEst, PVector pt, CircFieldObject& obj){
+      auto& groundToMid = obj.groundToMid;
+      double dFront = distEst / sensorHeight * (pt.y + groundToMid);
+      return distEst - dFront + pt.x;
+    }
+    static double getCenterObjectVertEstimateTop(double topEst, double sensorHeight, CircFieldObject& obj){
+      auto& topLeftBound = obj.topLeftBound;
+
+      double angle = atan(sensorHeight / topEst); // in rad
+      if(topLeftBound.size() != 1){
+        int i = 0;
+        while(90 - topLeftBound[i].angleTo(topLeftBound[i + 1]) > angle){
+          if(i == topLeftBound.size() - 1){
+            break;
+          }
+        }
+        if(i != 0 && abs(90 - topLeftBound[i].angleTo(topLeftBound[i + 1]) - angle) > abs(90 - topLeftBound[i - 1].angleTo(topLeftBound[i]) - angle)){
+          //Use btm
+          i--;
+        }
+        if(90 - topLeftBound[i].angleTo(topLeftBound[i + 1]) < angle){
+          //Change to top
+          i++;
+        }
+        auto pt = topLeftBound[i];
+        return getDistEst(sensorHeight, topEst, pt, obj);
+      }
+      else {
+        auto pt = topLeftBound[0];
+        return getDistEst(sensorHeight, topEst, pt, obj);
+      }
+    }
+    static double getCenterObjectVertEstimateBtm(double btmEst, double sensorHeight, CircFieldObject& obj){
+      auto& btmRghtBound = obj.btmRghtBound;
+
+      double angle = atan(sensorHeight / btmEst); // in rad
+      if(btmRghtBound.size() != 1){
+        int i = 0;
+        while(90 - btmRghtBound[i].angleTo(btmRghtBound[i + 1]) < angle){
+          if(i == btmRghtBound.size() - 1){
+            break;
+          }
+        }
+        if(i != 0 && abs(90 - btmRghtBound[i].angleTo(btmRghtBound[i + 1]) - angle) < abs(90 - btmRghtBound[i - 1].angleTo(btmRghtBound[i]) - angle)){
+          //Use btm
+          i--;
+        }
+        if(90 - btmRghtBound[i].angleTo(btmRghtBound[i + 1]) > angle){
+          //Change to top
+          i++;
+        }
+        auto pt = btmRghtBound[i];
+        return getDistEst(sensorHeight, btmEst, pt, obj);
+      }
+      else {
+        auto pt = btmRghtBound[0];
+        return getDistEst(sensorHeight, btmEst, pt, obj);
+      }
+    }
+    static PVector getCenterObjectHorEstimateLeft(double est, double sensorHeight, double distEst, double angleFromSensor, CircFieldObject& obj){
+      auto& topLeftBound = obj.topLeftBound;
+      auto& groundToMid = obj.groundToMid;
+      PVector farthestPt = {0, 0};
+      for(auto i : topLeftBound){
+        if(abs(i.x) > abs(farthestPt.x)){
+          farthestPt = i;
+        }
+      }
+      farthestPt.rotateXZ(-angleFromSensor);
+      double dist = PVector(est, distEst).mag();
+      double hypotenuse = PVector(dist, sensorHeight).mag();
+      double dFront = dist / sensorHeight * (farthestPt.y + groundToMid);
+      PVector endPt = {est, distEst};
+      PVector linePt = PVector(dFront, 0).rotate(endPt.heading2D() + 180);
+      endPt += linePt;
+      PVector posEst = endPt + PVector(farthestPt.x, farthestPt.z);
+      return posEst;
+    }
+    static PVector getCenterObjectHorEstimateRight(double est, double sensorHeight, double distEst, double angleFromSensor, CircFieldObject& obj){
+      auto& btmRghtBound = obj.btmRghtBound;
+      auto& groundToMid = obj.groundToMid;
+      PVector farthestPt = {0, 0};
+      for(auto i : btmRghtBound){
+        if(abs(i.x) > abs(farthestPt.x)){
+          farthestPt = i;
+        }
+      }
+      farthestPt.rotateXZ(angleFromSensor);
+      double dist = PVector(est, distEst).mag();
+      double hypotenuse = PVector(dist, sensorHeight).mag();
+      double dFront = dist / sensorHeight * (farthestPt.y + groundToMid);
+      PVector endPt = {est, distEst};
+      PVector linePt = PVector(dFront, 0).rotate(endPt.heading2D() + 180);
+      endPt += linePt;
+      PVector posEst = endPt + PVector(farthestPt.x, farthestPt.z);
+      return posEst;
+    }
+  public:
+    static PVector getRelPosLocation(PVector topLeft, PVector btmRight, double sensorHeight, double leftAngle, double rightAngle, CircFieldObject& obj){
+      double yEstTop = getCenterObjectVertEstimateTop(topLeft.y, sensorHeight, obj);
+      double yEstBtm = getCenterObjectVertEstimateBtm(btmRight.y, sensorHeight, obj);
+      double yEst = (yEstBtm + yEstTop) / 2.0;
+      PVector leftEst = getCenterObjectHorEstimateLeft(topLeft.x, sensorHeight, yEst, leftAngle, obj);
+      PVector rightEst = getCenterObjectHorEstimateRight(btmRight.x, sensorHeight, yEst, rightAngle, obj);
+      PVector estTotal = PVector(
+        (leftEst.x + rightEst.x) / 2.0, 
+        (leftEst.y + rightEst.y + yEst) / 3.0 // May want to weight yEst: 
+                                              //    ... + yEst * 2.0) / 4.0
+      );
+      return estTotal;
+    }
+  };
   PVector relPos;
   // static inline BasicWheelController* wc;
   static constexpr double 
@@ -81,6 +195,7 @@ public:
     vector<PVector> arr;
     for(int i = 0; i < sensor->objects.getLength(); i++){
       if(sensor->objects[i].exists){
+        auto pos = estimatePos(sensor->objects[i]);
         arr.push_back(estimatePos(sensor->objects[i]));
       }
     }
