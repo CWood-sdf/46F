98c5b5515f3369d3122186622ad47287c47c4a91
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-08-10 19:59:54 -0400

    Revert "Random stuff"
    This reverts commit fba4da472dc46ef3ed31567dfa0e8c3390f4e93d.

--- include/AutonInit/Init.h
+++ include/AutonInit/Init.h
@@ -54,11 +54,9 @@ public:
       return;
     }
     if(ready){
-      //Call selected auton
       auton->call();
     }
     else {
-      //Call potentiometer auton
       SelectorArr::getVal()();
     }
   }
@@ -66,26 +64,18 @@ public:
     return refList;
   }
   //Returns true when done
-  //This represents a pretty simple state machine
   static inline bool selectAuton(bool remake){
     //Create button list 
     if(buttons.size() == 0 && refList.size() != 0 && !autonSelected){
-      //Kinda hacky because we're using integer division
-      //If there is an odd number it will refList.size() / 2 will round down
-      //This is intended
       int buttonWidthCount = refList.size() - refList.size() / 2;
       bool isOddCount = refList.size() % 2 == 1;
-      double buttonWidth = (double)(BRAIN_WIDTH) / (double)buttonWidthCount;
+      double buttonWidth = (double)(BRAIN_WIDTH - 80) / (double)buttonWidthCount;
       double buttonHeight = (double)(BRAIN_HEIGHT + 20) / 2.0;
-      //Just fill buttons
       for(int i = 0; i < refList.size(); i++){
-        //Once again integer division is intended
-        buttons.push_back(new Button(Brain, (i / 2) * buttonWidth, (i % 2) * buttonHeight, buttonWidth, buttonHeight, vex::blue, vex::purple, refList[i]->name, -30, -30));
+        buttons.push_back(new Button(Brain, (i / 2) * buttonWidth + 40, (i % 2) * buttonHeight, buttonWidth, buttonHeight, vex::blue, vex::purple, refList[i]->name, -30, -30));
       }
     }
     Brain.Screen.clearScreen(black);
-    //This is first stage
-    //Just draw the buttons and see if they're clicked
     if(!autonSelected){
       for(int i = 0; i < buttons.size(); i++){
         buttons[i]->draw();
@@ -95,46 +85,38 @@ public:
         }
       }
     }
-    //If auton is not confirmed
     else if(!ready){
       confirm.draw();
       deny.draw();
       Brain.Screen.setFillColor(black);
       Brain.Screen.printAt(BRAIN_WIDTH / 2 - 20, BRAIN_HEIGHT / 2, auton->name.data());
       //Deny first as an extra layer of safety
-      //Idk if this safety really helps at all, but whatevs
       if(deny.clicked()){
         //Send to top again
         autonSelected = false;
       }
       else if(confirm.clicked()){
-        //Lock in current choice
         ready = true;
         //Delete buttons
         for(auto b : buttons){
           delete b;
         }
         buttons.clear();
-        //Return true, this line is technically unnecessary, but whatevs
         return true;
       }
 
     }
     return ready;
   }
-  //Easily make an auton function
-  //Works more like +=
   Auton& operator+(std::function<void()> fn){
     setFn(fn);
     potGet.attachFn(fn);
     return *this;
   }
-  //Are we ready?
   static bool isDone(){
     return ready;
   }
 };
-//Operators for easily making auton functions
 Auton operator "" _afn(const char* s){
   return Auton(s, [](){});
 }
