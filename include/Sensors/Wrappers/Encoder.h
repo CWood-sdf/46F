881697db1644423791dfc16393dca9cf73ccb60e
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-09-06 18:57:04 -0400

    Edit Encoder.h

--- include/Sensors/Wrappers/Encoder.h
+++ include/Sensors/Wrappers/Encoder.h
@@ -10,23 +10,104 @@
 //triport and __tridevice (vex_triport.h)
 //Will prolly need a different hack for triport devices rather than the normal ones
 class Encoder {
-    V5_DeviceType encoderType = V5_DeviceType::kDeviceTypeMotorSensor;
-    void* encoder;
-    static inline devices device = devices();
+    // encoder sdfsd = encoder(Brain.ThreeWirePort.A);
+    // const V5_DeviceType encoderType = V5_DeviceType::kDeviceTypeMotorSensor;
+    std::function<double(rotationUnits)> getValue;
+    std::function<void()> resetter;
+    // static inline devices device = devices();
 public:
-    //A struct that gets the type of the encoder from the V5_DeviceType
-    template<V5_DeviceType DeviceType>
-    struct GetType {
-        typedef void type;
+    // SFINAE test
+    template <typename T>
+    class HasResetPosition
+    {
+    private:
+        typedef char YesType[1];
+        typedef char NoType[2];
+
+        template <typename C> static YesType& test(decltype(&C::resetPosition));
+        template <typename C> static NoType& test(...);
+
+
+    public:
+        enum { value = sizeof(test<T>(0)) == sizeof(YesType) };
     };
-    template<V5_DeviceType DeviceType>
-    typename Encoder::GetType<DeviceType>::type* getEncoder() {
-        return (typename Encoder::GetType<DeviceType>::type*)encoder;
+    //The constructor, only enable if the type has been mapped
+    template<class Sensor>
+    Encoder(Sensor* e, typename std::enable_if<HasResetPosition<Sensor>::value, std::string>::type sdf = "") {
+        getValue = [e](rotationUnits units) {
+            return e->position(units);
+        };
+        resetter = [e]() {
+            e->resetPosition();
+        };
     }
-    
+    template<class Sensor>
+    Encoder(Sensor* e, typename std::enable_if<!HasResetPosition<Sensor>::value, std::string>::type sdf = "") {
+        getValue = [e](rotationUnits units) {
+            return e->position(units);
+        };
+        resetter = [e]() {
+            e->resetRotation();
+        };
+    }
+    template<class Sensor>
+    Encoder(Sensor& e) : Encoder(&e) {
+    }
+    Encoder() {
+        getValue = [](rotationUnits units) {
+            return 0;
+        };
+        resetter = [](void) {
+            
+        };
+    }
+    //Make the position method
+    double position(rotationUnits units) {
+        return getValue(units);
+    }
+    void resetPosition() {
+        resetter();
+    }
+    Encoder& operator=(const Encoder& other) {
+        getValue = other.getValue;
+        return *this;
+    }
+    Encoder& operator=(Encoder&& other) {
+        getValue = other.getValue;
+        return *this;
+    }
 };
-template<>
-struct Encoder::GetType<V5_DeviceType::kDeviceTypeMotorSensor> {
-    typedef motor type;
-};
+// //Matches kDeviceTypeMotorSensor to motor encoders
+// template<>
+// struct Encoder::GetType<V5_DeviceType::kDeviceTypeMotorSensor> {
+//     typedef motor type;
+// };
+// template<>
+// struct Encoder::GetDeviceType<motor> {
+//     static constexpr V5_DeviceType deviceType = V5_DeviceType::kDeviceTypeMotorSensor;
+// };
+// //Matches kDeviceTypeAbsEncSensor to rotation sensor
+// template<>
+// struct Encoder::GetType<V5_DeviceType::kDeviceTypeAbsEncSensor> {
+//     typedef rotation type;
+// };
+// template<>
+// struct Encoder::GetDeviceType<rotation> {
+//     static constexpr V5_DeviceType deviceType = V5_DeviceType::kDeviceTypeAbsEncSensor;
+// };
+// //Matches kDeviceTypeUndefinedSensor to three wire encoder
+// template<>
+// struct Encoder::GetType<V5_DeviceType::kDeviceTypeUndefinedSensor> {
+//     typedef encoder type;
+// };
+// template<>
+// struct Encoder::GetDeviceType<encoder> {
+//     static constexpr V5_DeviceType deviceType = V5_DeviceType::kDeviceTypeUndefinedSensor;
+// };
+#ifndef NO_MAKE
+motor* sdfsdf = nullptr;
+Encoder* globalEnc = new Encoder(sdfsdf);
+encoder* sdfdfsd = nullptr;
+Encoder* globalEnc2 = new Encoder(sdfdfsd);
 #endif
+#endif  // ENCODER_H
